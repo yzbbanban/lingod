d504b061429db1315f9bd2fab52b2878bc3c7b2b
yzbbanban <yzbbanban@live.com>
2019-12-14 11:38:00 +0800

    fiajwoefjwe

--- src/main/java/com/as/lingod/controller/FaProductLingoController.java
+++ src/main/java/com/as/lingod/controller/FaProductLingoController.java
@@ -70,6 +70,49 @@ public class FaProductLingoController {
 
     }
 
+    @PostMapping("delAndSave")
+    @Transactional(rollbackFor = Exception.class)
+    public ResultJson<String> delAndSaveLingoResult(String faCalcJson, String faLingoJson) {
+        Gson gson = new Gson();
+        FaProductLingoCalc faCalc = gson.fromJson(faCalcJson, new TypeToken<FaProductLingoCalc>() {
+        }.getType());
+        List<FaProductLingo> faLingoList = gson.fromJson(faLingoJson, new TypeToken<List<FaProductLingo>>() {
+        }.getType());
+        Map<String, Object> map = new HashMap<>(4);
+        map.put("edition", faCalc.getEdition());
+        map.put("protype", faCalc.getProtype());
+        map.put("totalpeo", faCalc.getTotalpeo());
+        map.put("nameId", faCalc.getNameId());
+        List<FaProductLingoCalc> faLingoCalc = faProductLingoCalcService.selectByMap(map);
+        //删除原有记录
+        Integer cId = faLingoCalc.get(0).getId();
+        if (!faProductLingoCalcService.deleteById(cId)) {
+            throw new RuntimeException("error");
+        }
+        //查询原有具体记录
+        Map<String, Object> calc = new HashMap<>(4);
+        calc.put("calcId", cId);
+        List<FaProductLingo> faLingo = faProductLingoService.selectByMap(calc);
+        for (int i = 0; i < faLingo.size(); i++) {
+            if (!faProductLingoService.deleteById(faLingo.get(i).getId())) {
+                throw new RuntimeException("error");
+            }
+        }
+
+        boolean r = faProductLingoCalcService.insertAllColumn(faCalc);
+        //先设置calc id
+        Integer calcId = faCalc.getId();
+        for (int i = 0, len = faLingoList.size(); i < len; i++) {
+            FaProductLingo faProductLingo = faLingoList.get(i);
+            faProductLingo.setCalcId(calcId);
+        }
+        boolean row = faProductLingoService.insertBatch(faLingoList);
+        if (row && r) {
+            return ResultJson.createBySuccess();
+        }
+        throw new RuntimeException("error");
+
+    }
 
 }
 
